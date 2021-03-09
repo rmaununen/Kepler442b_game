4cc2d67ab793236ad40cc6997211d59c8166ba95
Rudolf Maununen <80330674+rmaununen@users.noreply.github.com>
2021-03-09 14:39:50 +0100

    here the initial values for variables are set

---
+++ initial_values.py
@@ -0,0 +1,129 @@
+#TIME TRACKING VARIABLES
+FPS = 30
+a = 10
+b = 0
+c = 0
+d = 0
+e = 0
+f = 0
+g = 0
+j = 0
+k = 0
+iso_time = 14
+iso_timex = 0
+
+
+
+
+
+#VARIABLES USED IN show_menu() FUNCTION
+cy = 200
+up = True
+dy = 230
+up2 = False
+dpy = 0
+dpx = 0
+cpy = 0
+cpx = 0
+
+#FOR MENUS
+run = True
+neo_b = False
+shmenu = True
+shmenu1 = False
+shsettings = False
+shintro = False
+coffin = False
+finish = False
+question = False
+expl_atl = False
+Mode1 = False
+sliding1 = False
+sliding2 = False
+tut = False
+
+#FOR ANIMATIONS
+expl_b = True 
+shintro_count = 1
+explosion_time = 0
+xpl_count = 0
+cof_start_time = 0
+animcount_c = 0
+fire_animcount = 0
+blcount = 0
+kemcount = 0
+smoke_count = 0
+re_animcount = 0
+atlcount = 0
+atl_jumpcount = 0
+neo_count = 0
+expl_atl_count = 0
+termcount = 0
+term_jumpcount = 0
+sit_count = 0
+tut_count = 1
+xen_count = 0
+r_speed = 0
+animcount = 0
+
+#LEFT PLAYER
+x = 50 
+y = 320
+width = 65
+height = 135
+speed = 5
+jumpSpeed = 7
+facing = 1
+health = 100
+red=0
+green=255
+walking = False
+isJump = False
+p_blast_possible = False
+intersect1=False
+locked = False
+p_blaster = []
+kamni = []
+kamni_inner_list = []
+
+#RIGHT PLAYER
+angle = 0
+x_of = 0
+y_of = 0
+yofs = 0
+x1 = 590
+x1t = 630
+y1 = 327 
+width1 = 80
+height1 = 135
+health1 = 10
+health_d = 100
+health_kem = 100
+health_cor = 280
+coronaheight = 118
+coronawidth = 110
+jumpSpeed1 = 8
+speed1 = 9
+red1=0
+green1=255
+fight = False
+isJump1 = False
+moving = False
+sit = False
+kinul = False
+kinul2 = False
+intersect=False
+isolation = False
+locked = False
+cof_right = False
+home = 0
+kamni1 = []
+kamni2 = []
+rakety = []
+viruses = []
+
+
+
+
+
+
